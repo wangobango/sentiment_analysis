fa74dd6dbb61735e95655c1a018c120594431b7c
Jacek Maciupa <spaghetti147@gmail.com>
2019-10-13 22:12:06 +0200

    Parser

---
+++ data_loader.py
@@ -0,0 +1,34 @@
+import xml.etree.ElementTree as ET
+
+
+class Sentence:
+
+    def __init__(self, sid, domain, polarity, summary, text):
+        self.id = sid
+        self.domain = domain
+        self.polarity = polarity
+        self.summary = summary
+        self.text = text
+
+
+class DataLoader:
+
+    def __init__(self):
+        self.path = None
+        self.data = None
+
+    def set_path(self, path):
+        self.path = path
+
+    def read_xml(self):
+        if self.path is None:
+            return "path is not set!"
+        sentences = []
+        tree = ET.parse(self.path)
+        root = tree.getroot()
+
+        for child in root:
+            sentences.append(Sentence(child.attrib.get("id"), child.find("domain").text, child.find("polarity").text,
+                                      child.find("summary").text, child.find("text").text))
+
+        return sentences
