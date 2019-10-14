f7189a4a28dcc28ed5f559aa44fe8ce433400885
wangobango <puch003@wp.pl>
2019-10-14 14:13:57 +0200

    fix

---
+++ data_exploration.py
@@ -0,0 +1,18 @@
+from data_loader import DataLoader
+from config import Config
+from pprint import pprint
+
+PROP = "CURRENT_DATA"
+VALUE = "./data/Amazon_Instant_Video/Amazon_Instant_Video.neg.0.xml"
+
+if __name__ == "__main__":
+    loader = DataLoader()
+    # loader.read_xml()
+
+    conf = Config()
+    conf.addProperty(PROP,VALUE)
+
+    loader.set_path(conf.readValue(PROP))
+
+    data = loader.read_xml()
+    print(data)
