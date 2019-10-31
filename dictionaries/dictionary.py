d1b0b8f9f54e84434df5b399dce40e865c686a5f
Jacek Maciupa <spaghetti147@gmail.com>
2019-10-31 23:21:21 +0100

    dictionary reader

--- dictionaries/dictionary.py
+++ dictionaries/dictionary.py
@@ -1,4 +1,4 @@
-from .senticnet.senticnet import SenticNet
+from senticnet.senticnet import SenticNet
 
 """Class which uses Senticnet API dictionary. Full documentation here: https://sentic.net/api/"""
 
@@ -7,23 +7,31 @@ class Dictionary:
 
     def __init__(self):
         self.sn = SenticNet()
+
     """
         Input : String 
         Output : "positive" or "negative"
     """
 
-    def get_word_polarity(self, word, binary=False):
-        return self.sn.polarity_value(word)
+    def get_word_polarity(self, word):
+        value = "empty"
+        try:
+            value = self.sn.polarity_value(word.lower())
+        except:
+            print('An error occurred. Word: ' + word + 'is not known.')
+
+        return value
+
+    """
+        Input : String
+        Output : Int [-1 : 1]    
+    """
 
     def get_word_polarity_numerical_value(self, word):
-        return self.sn.polarity_intense(word)
+        value = "empty"
+        try:
+            value = self.sn.polarity_intense(word.lower())
+        except:
+            print('An error occurred. Word: ' + word + 'is not known.')
+        return value
 
-
-def main():
-    sn = Dictionary()
-
-    print(sn.get_word_polarity('Amazing'))
-
-
-if __name__ == "__main__":
-    main()
