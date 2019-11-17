586af6fb3e8cecf7dd75d948fe3c9e0b74c4e240
wangobango <puch003@wp.pl>
2019-11-17 20:49:24 +0100

    init

---
+++ utils/process_results.py
@@ -0,0 +1,25 @@
+import json
+from .config import Config
+from pprint import pprint
+
+class ResultsProcessor:
+    def __init__(self):
+        self.config = Config()
+
+    def loadResults(self):
+        path = self.config.readValue("results_path")
+        with (open(path, "r")) as f:
+            self.results = json.load(f)
+    
+    def getValueFromAllDomains(self, value):
+        results = {}
+        for (key, val) in self.results.items():
+            results[key] = val[value]
+        pprint(results)
+        return results
+
+
+if __name__ == "__main__":
+    rp = ResultsProcessor()
+    rp.loadResults()
+    rp.getValueFromAllDomains("personCorBetweenTextLengthAndPolarity")
