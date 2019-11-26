63267b8ec892fab3e3f064d85045359e385eb9b1
uini223 <bartnik.emil@gmail.com>
2019-11-26 12:27:56 +0100

    added shuffle flag

--- utils/data_reader.py
+++ utils/data_reader.py
@@ -15,15 +15,17 @@ class DataReader:
         self.test_set_file_path = self.config.readValue(TEST_SET_PATH)
 
 
-    def read_data_set(self):
+    def read_data_set(self, shuffle=True):
         LOGGER.info("Reading data set...")
         return self.read_set(self.data_set_file_path)
 
-    def read_test_set(self):
+    def read_test_set(self, shuffle=True):
         LOGGER.info("Reading test set...")
         return self.read_set(self.test_set_file_path)   
 
-    def read_set(self, data_set_file):
-        dset = utils.shuffle(pd.read_csv(data_set_file))
-        dset.reset_index(inplace=True, drop=True)
+    def read_set(self, data_set_file, shuffle=True):
+        dset = pd.read_csv(data_set_file)
+        if shuffle:
+            dset = utils.shuffle(dset)
+            dset.reset_index(inplace=True, drop=True)
         return dset
