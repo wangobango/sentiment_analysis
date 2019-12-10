f264f03b33a629146c1a019e47aae75a38cb5f08
uini223 <bartnik.emil@gmail.com>
2019-12-10 16:03:48 +0100

    finished setup.py script

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 import os
 import subprocess
+from utils.preprocessor import Preprocessor
 
 DATA_FOLDER="data"
+AGGREGATED="aggregated"
+
 if __name__ == "__main__":
     if not DATA_FOLDER in os.listdir():
         subprocess.run(["./download_dataset.sh"])
-    
+    if not AGGREGATED in os.listdir():
+        os.mkdir(AGGREGATED)
+        Preprocessor.aggregateData()
