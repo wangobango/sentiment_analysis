6af1a5be91b7bd17b9fbc0295745ec9ad2904236
uini223 <bartnik.emil@gmail.com>
2020-01-03 00:44:11 +0100

    moreeee code, isn't that beautiful ?

--- huggingfaces/data_converter.py
+++ huggingfaces/data_converter.py
@@ -1,4 +1,9 @@
 from utils.config import Config
+from .data_preparator import LABEL_MAP
+from multiprocessing import Pool, cpu_count
+from tqdm import tqdm
+from pytorch_pretrained_bert import BertTokenizer
+import pickle, copy
 import sys,csv
 
 class InputExample():
@@ -8,14 +13,23 @@ class InputExample():
         self.text_b = text_b
         self.label = label
 
+class InputFeatures(object):
+    """A single set of features of data."""
+
+    def __init__(self, input_ids, input_mask, segment_ids, label_id):
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.label_id = label_id
 
 class BertDataConverter():
     def __init__(self):
         self.config = Config()
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=False)
 
     def get_dev_examples(self):
         dev_set = self.read_tsv(self.config.readValue('bert_dev_set'), "dev")
-        print(dev_set)
+        return dev_set
 
     @staticmethod
     def read_tsv(input_file, set_type,quotechar=None):
@@ -31,5 +45,37 @@ class BertDataConverter():
                 lines.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
             return lines
 
+    def convert_example_to_features(self, example):
+        max_seq_length = 512
+        tokenizer = self.tokenizer
+        tokens = tokenizer.tokenize(example.text_a)
+        if len(tokens) > max_seq_length - 2: # -2 for [CLS] and [SEP]
+            tokens = tokens[:(max_seq_length - 2)]
+        tokens = ["[CLS]"] + tokens + ["[SEP]"]
+        segment_ids = [0] * len(tokens)
+        input_ids = tokenizer.convert_tokens_to_ids(tokens)
+        input_mask = [1] * len(tokens)
+        padding = [0] * (max_seq_length - len(input_ids))
+        input_ids += padding
+        input_mask += padding
+        segment_ids += padding
+        label_id = example.label
+        return InputFeatures(input_ids=input_ids, input_mask=input_mask, segment_ids=segment_ids, label_id=label_id)
+
+def convert_examples_to_features(bertDataConverter, examples):
+    process_count = cpu_count() - 1
+    with Pool(process_count) as p:
+        train_features = \
+            list(tqdm(p.imap(bertDataConverter.convert_example_to_features, examples), total=len(examples)))
+    with open('./huggingfaces' + 'train_features.pkl', "wb") as f:
+        pickle.dump(train_features, f)
+
 if __name__ == "__main__":
-    BertDataConverter().get_dev_examples()
+    bertDataConverter = BertDataConverter()
+    examples = bertDataConverter.get_dev_examples()
+    convert_examples_to_features(bertDataConverter, examples)
+
+
+#MAX sequence length
+#LABEL MAP
+#tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=False)
--- huggingfaces/data_preparator.py
+++ huggingfaces/data_preparator.py
@@ -1,19 +1,20 @@
 from utils.data_reader import DataReader
 from utils.config import Config
 import pandas as pd
+LABEL_MAP = {'negative': 0, 'positive': 1}
 
 # Class for prepering csv data into tsv data
 class BertDatapreparator():
     def __init__(self):
         self.dataReader = DataReader()
         self.config = Config()
-        self.labelDict = {'negative': 0, 'positive': 1}
+        self.labelMap = LABEL_MAP
 
     def prepareDevset(self):
         dataSet = self.dataReader.read_data_set()
         dev_df_bert = pd.DataFrame({
             'id': range(len(dataSet)),
-            'label':dataSet['polarity'].replace(self.labelDict),
+            'label':dataSet['polarity'].replace(self.labelMap),
             'alpha':['a']*dataSet.shape[0],
             'text': dataSet['text'].replace(r'\n', ' ', regex=True)
         })
@@ -24,7 +25,7 @@ class BertDatapreparator():
         testSet = self.dataReader.read_data_set()
         test_df_bert = pd.DataFrame({
             'id': range(len(testSet)),
-            'label':testSet['polarity'].replace(self.labelDict),
+            'label':testSet['polarity'].replace(self.labelMap),
             'alpha':['a']*testSet.shape[0],
             'text': testSet['text'].replace(r'\n', ' ', regex=True)
         })
